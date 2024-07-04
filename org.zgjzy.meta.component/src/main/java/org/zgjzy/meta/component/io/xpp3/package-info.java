89a41cc9b463eb3ee981d3bfd691c9e729ec4ffb
Qiu Kuining <qiukuining@163.com>
2024-07-04 11:05:47 +0800

    首次更新代码

---
+++ org.zgjzy.meta.component/src/main/java/org/zgjzy/meta/component/io/xpp3/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 模型元数据XML文件读写软件，代码产生由 <code>model-meta.mdo</code> 生成。
+ * 内部XML处理使用了
+ * <a href="https://codehaus-plexus.github.io/plexus-utils/apidocs/org/codehaus/plexus/util/xml/pull/package-summary.html">plexus-utils的
+ * XML拉式解析器</a> ，以及
+ * <a href="https://codehaus-plexus.github.io/plexus-utils/apidocs/org/codehaus/plexus/util/xml/Xpp3DomBuilder.html">Xpp3DomBuilder</a> +
+ * <a href="https://codehaus-plexus.github.io/plexus-utils/apidocs/org/codehaus/plexus/util/xml/Xpp3Dom.html">Xpp3Dom</a> for
+ * DOM内容表示（见 <code>&lt;configuration&gt;</code> 元素。
+ */
+package org.zgjzy.meta.component.io.xpp3;
---
+++ org.zgjzy.meta.model/src/main/java/org/zgjzy/meta/model/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * 中国建筑业协会标准《建筑信息模型数字成果元数据标准》，模型元数据核心类目录，其类定义在 <code>model-meta.mdo</code> 中定义，并由工具自动生成。
+ * 核心类是{@link org.zgjzy.meta.model.Model}.
+ */
+package org.zgjzy.meta.model;
